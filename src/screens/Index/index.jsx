426fe000272ebcb58b08fa2e311aea6cfe895cca
Lucas Medeiros <medeiros.lucas1993@gmail.com>
2022-10-06 22:23:00 -0300

    recebendo generos da API

--- src/screens/Index/index.jsx
+++ src/screens/Index/index.jsx
@@ -1,15 +1,32 @@
 import { Picker } from "@react-native-picker/picker";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Text, TextInput, View } from "react-native";
 
 export function Index() {
-  const genres = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [genres, setGenres] = useState(["selecione"]);
 
   const [selectedGenre, setSelectedGenre] = useState("selecione");
 
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": process.env.API_KEY,
+        "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+      },
+    };
+
+    fetch("https://moviesdatabase.p.rapidapi.com/titles/utils/genres", options)
+      .then((response) => response.json())
+      .then((response) => {
+        setGenres(response.results);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   const options = genres.map((item) => {
-    const label = item.toString();
-    return <Picker.Item label={label} value={item} key={item} />;
+    const label = item == null ? "selecione" : item.toString();
+    return <Picker.Item label={label} value={label} key={label} />;
   });
 
   return (
